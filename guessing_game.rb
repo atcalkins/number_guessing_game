29bab1191c65a66bd15e4b34b934f135ca53e630
Tyler Calkins <atcalkins@outlook.com>
2017-09-14 16:12:25 -0400

    install rubocop linter fix indentation and game start print in while loop

--- guessing_game.rb
+++ guessing_game.rb
@@ -11,26 +11,23 @@ def user_guess
 end
 
 while attempts < 5
-  print game_start
+  num = user_guess
+  if guesses.include? num
+    puts "Number has already been guessed."
+  end
 
-num = user_guess
-  #puts a message here to show they guessed
-if guesses.include? num
-  puts "Number has already been guessed."
+  guesses.push num
+
+  if num == rand_num
+    puts "Correct! You win!!!!!"
+    exit
+  elsif num < rand_num
+    puts "Number guessed was too low, please select a higher number"
+    attempts += 1
+  elsif num > rand_num
+    puts "Number guessed was too high, please select a lower number"
+    attempts += 1
+  end
 end
 
-guesses.push num
-
-if num == rand_num
-  puts "Correct! You win!!!!!"
-  exit
-elsif num < rand_num
-  puts "Number guessed was too low, please select a higher number"
-  attempts += 1
-elsif num > rand_num
-  puts "Number guessed was too high, please select a lower number"
-  attempts += 1
-end
-end
-# if they lost print losing message here?
 puts "You have run out of attempts, the number was #{rand_num} please come back and play again."
