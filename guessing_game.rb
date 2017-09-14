e23a265eeb39cb39e5ca2258f51b203b523430bb
Tyler Calkins <atcalkins@outlook.com>
2017-09-14 11:50:20 -0400

    fix indentation and move method out of while loop

--- guessing_game.rb
+++ guessing_game.rb
@@ -2,38 +2,35 @@ rand_num = rand(1..100)
 attempts = 0
 guesses = []
 
-  def game_start
-    puts "Please choose a number between 1 and 100.\n"
-  end
+def game_start
+  puts "Please choose a number between 1 and 100.\n"
+end
 
-  while attempts < 5
-    print game_start
+def user_guess
+  gets.chomp.to_i
+end
 
-  def user_guess
-    gets.chomp.to_i
-  end
+while attempts < 5
+  print game_start
 
-  num = user_guess
-
+num = user_guess
   #puts a message here to show they guessed
-  if guesses.include? num
-    puts "Number has already been guessed."
-  end
+if guesses.include? num
+  puts "Number has already been guessed."
+end
 
-  guesses.push num
+guesses.push num
 
-  if num == rand_num
-    puts "Correct! You win!!!!!"
-    exit
-
-  elsif num < rand_num
-    puts "Number guessed was too low, please select a higher number"
-    attempts += 1
-
-  elsif num > rand_num
-    puts "Number guessed was too high, please select a lower number"
-    attempts += 1
-  end
+if num == rand_num
+  puts "Correct! You win!!!!!"
+  exit
+elsif num < rand_num
+  puts "Number guessed was too low, please select a higher number"
+  attempts += 1
+elsif num > rand_num
+  puts "Number guessed was too high, please select a lower number"
+  attempts += 1
+end
 end
 # if they lost print losing message here?
 puts "You have run out of attempts, the number was #{rand_num} please come back and play again."
